20f45a4945b83eaab07b815a7f6bd663725db0e4
pacho ruiz <alfrrugu@hotmail.com>
2025-10-27 22:10:18 -0600

    Stats View controller

---
+++ AppCuidadoAgua/ViewControllerStats.h
@@ -0,0 +1,17 @@
+//
+//  ViewControllerStats.h
+//  AppCuidadoAgua
+//
+//  Created by Alejandro Francisco Ruiz Guerrero on 27/10/25.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ViewControllerStats : UIViewController
+@property (weak, nonatomic) IBOutlet UIView *chartContainerView;
+
+@end
+
+NS_ASSUME_NONNULL_END
